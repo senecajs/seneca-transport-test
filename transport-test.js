b9532be0959b258447e850cc91cbdf88817de918
Richard Rodger <richard@ricebridge.com>
2014-07-01 16:54:22 +0100

    bump 0.1.1, fault tolerance test

--- transport-test.js
+++ transport-test.js
@@ -5,6 +5,9 @@
 var assert = require('assert')
 
 
+// TODO: also test fire-and-forget
+
+
 function foo_plugin() {
   this.add( 'foo:1', function(args,done){done(null,{dee:'1-'+args.bar})} )
   this.add( 'foo:2', function(args,done){done(null,{dee:'2-'+args.bar})} )
@@ -70,7 +73,76 @@ function foo_test( require, fin, type, port ) {
 
 
 
+// TODO: test fire-and-forget
+
+
+function foo_fault( require, type, port, speed ) {
+  var speed = speed ? parseInt(speed,10) : 2
+
+  var service = foo_service(
+    require('seneca')({log:'silent'}).use('..'), 
+    type, port)
+
+  service.ready( function(){
+    var client = 
+          require('seneca')({log:'silent'})
+          .use('..')
+          .client({type:type,port:port}) 
+
+    var i = 0;
+    console.log('CALLA '+i)
+    client.act('foo:1,bar:'+i, console.log)
+    i++
+
+    var c0 = setInterval( function(){
+      console.log('CALLA '+i)
+      client.act('foo:1,bar:'+i, console.log)
+      i++
+    },1000/speed)
+
+    setTimeout( function(){
+      console.log('CLOSE SERVER')
+      service.close(console.log)
+      i = 100
+    }, 5000/speed )
+
+    setTimeout( function(){
+      console.log('RESTART SERVER')
+      service = foo_service(
+        require('seneca')({log:'silent'}).use('..'), 
+        type, port)
+    }, 9000/speed )
+
+
+    setTimeout( function(){
+      console.log('NEW CLIENT')
+      client.close(console.log)
+      clearInterval(c0)
+      i = 1000
+
+      client = 
+        require('seneca')({log:'silent'})
+        .use('..')
+        .client({type:type,port:port}) 
+      
+      setInterval( function(){
+        console.log('CALLB '+i)
+        client.act('foo:1,bar:'+i, console.log)
+        i++
+      },1000/speed)
+
+    }, 14000/speed )
+
+    setTimeout( function(){
+      console.log('KILL AND RESTART TRANSPORT MANUALLY...')
+    }, 19000/speed )
+
+  })
+}
+
+
 
 module.exports = {
-  foo_test: foo_test
+  foo_test: foo_test,
+  foo_fault: foo_fault,
 }
